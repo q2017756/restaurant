8f6d76b150e59ff65f7c93eb5ec4c0d4d12534c7
Unknown <491397425@qq.com>
2018-08-05 11:40:55 +0800

    [add] 日历组件

---
+++ src/components/AppCalendar/calendar.vue
@@ -0,0 +1,145 @@
+<template>
+    <div class="schedule-calendar" @wheel="test">
+        <header-bar ref="header" :year="year"
+                    :month="month"
+                    :day="day"
+                    :currentMonth="currentMonth"
+                    @updateValue="updateView"></header-bar>
+        <div class="schedule-calendar-body">
+            <week :startWeek="startWeek"></week>
+            <month :year="year"
+                   :month="month"
+                   :startWeek="startWeek"
+                   :data="events"
+                   :itemRender="dateItemRender"></month>
+        </div>
+    </div>
+</template>
+<script>
+import { EventBus } from './utils'
+import headerBar from './header'
+import week from './week'
+import month from './month'
+
+export default {
+    name: 'schedule-calendar',
+    components: {
+        headerBar,
+        week,
+        month,
+    },
+    props: {
+        startMonth: String,
+        startWeek: {
+            type: Number,
+            default: 7
+        },
+        events: {
+            type: Array,
+            default: []
+        },
+        dateItemRender: Function
+    },
+    data() {
+        return {
+            year: new Date().getFullYear(),
+            month: new Date().getMonth(),
+            day: new Date().getDate(),
+            currentMonth: new Date().getMonth(),
+            dragItem: null
+        }
+    },
+    methods: {
+        updateView(year, month) {
+            this.year = year
+            this.month = month
+        },
+        cellDragenter(e, date, type, index) {
+            this.$emit('event-dragenter', e, this.dragItem, date)
+        },
+        itemDragstart(e, item, date, type) {
+            this.dragItem = item
+            this.$emit('event-dragstart', e, item, date)
+        },
+        itemDrop(e, date, type, index) {
+            if (!this.dragItem) return
+            console.log('[event-dragend]:', this.dragItem, date)
+            this.$emit('event-dragend', e, this.dragItem, date)
+        },
+        itemClick(e, item) {
+            console.log('[event-click]:', item)
+            this.$emit('event-click', e, item)
+        },
+        dateClick(e, date) {
+            console.log('[date-click]:', date)
+            this.$emit('date-click', e, date)
+        },
+        test(e) {
+            e = e || window.event;
+            if (e.wheelDelta) {  //判断浏览器IE，谷歌滑轮事件
+                if (e.wheelDelta > 0) { //当滑轮向上滚动时
+                    this.$refs.header.prevMonth();
+                }
+                if (e.wheelDelta < 0) { //当滑轮向下滚动时
+                    this.$refs.header.nextMonth();
+                }
+            } else if (e.detail) {  //Firefox滑轮事件
+                if (e.detail> 0) { //当滑轮向上滚动时
+                    this.$refs.header.prevMonth();
+                }
+                if (e.detail< 0) { //当滑轮向下滚动时
+                    this.$refs.header.nextMonth();
+                }
+            }
+        }
+    },
+    created() {
+        EventBus.$on('cell-dragenter', this.cellDragenter)
+        EventBus.$on('item-dragstart', this.itemDragstart)
+        EventBus.$on('item-drop', this.itemDrop)
+        EventBus.$on('item-click', this.itemClick)
+        EventBus.$on('date-click', this.dateClick)
+    },
+    destoryed() {
+        EventBus.$off()
+    }
+}
+</script>
+<style lang="scss">
+@import "variables.scss";
+
+.schedule- {
+    &calendar {
+        position: relative;
+        display: flex;
+        flex-direction: column;
+        width: 100%;
+        height: 80%;
+        min-width: $sc-cell-min-width * 7;
+        color: $sc-base-color;
+        font-size: $sc-base-font-size;
+
+        *,
+        *::before,
+        *::after {
+            box-sizing: border-box;
+        }
+
+        button {
+            border: 0;
+            outline: none;
+            cursor: pointer;
+            background: transparent;
+        }
+
+        &-body {
+            position: relative;
+            flex: 1;
+            width: 100%;
+            overflow: hidden;
+            background: $sc-body-color;
+        }
+    }
+}
+
+</style>
---
+++ src/components/AppCalendar/header.vue
@@ -0,0 +1,130 @@
+<template>
+    <div class="header">
+        <div class="header-top">
+            <div class="year-selector">
+                <img src="../../assets/img/arrow-left.png" alt="" @click="prevYear">
+                <p>{{year}} 年</p>
+                <img src="../../assets/img/arrow-right.png" alt="" @click="nextYear">
+            </div>
+            <p>
+                {{year}} 年 {{month + 1}} 月 {{day}} 日
+            </p>
+        </div>
+        <ul class="month-selector">
+            <li v-for="n in 12"
+                :class="{prev: n < (currentMonth + 1), current: n === currentMonth + 1, active: n === month + 1}"
+                @click="updateValue(year,n - 1)">{{n}}</li>
+        </ul>
+    </div>
+</template>
+<script>
+import { calcPrevMonth, calcNextMonth } from './utils'
+import picker from './picker'
+
+export default {
+    components: {
+        picker
+    },
+    props: {
+        year: Number,
+        month: Number,
+        currentMonth: Number,
+        day: Number
+    },
+    data() {
+        return {
+            pickerVisible: false,
+        }
+    },
+    computed: {
+
+    },
+    methods: {
+        updateValue(year, month = this.month) {
+            this.$emit('updateValue', year, month)
+        },
+        prevYear() {
+            this.updateValue(this.year - 1)
+        },
+        nextYear() {
+            this.updateValue(this.year + 1)
+        },
+        prevMonth() {
+            const { year, month } = calcPrevMonth(this.year, this.month)
+            this.updateValue(year, month)
+        },
+        nextMonth() {
+            const { year, month } = calcNextMonth(this.year, this.month)
+            this.updateValue(year, month)
+        },
+        clickOutSide(e) {
+            if (this.pickerVisible && !this.$refs.picker.contains(e.target)) {
+                this.pickerVisible = false
+            }
+        }
+    },
+
+    created() {
+        document.addEventListener('mouseup', this.clickOutSide)
+    },
+    destoryed() {
+        document.removeEventListener('mouseup', this.clickOutSide)
+    }
+}
+</script>
+<style scoped lang="scss">
+@import "variables.scss";
+
+.header-top {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    font-size: 24px;
+    .year-selector{
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        img {
+            padding: 20px;
+            &:first-child {
+                padding-left: 0;
+            }
+        }
+    }
+}
+ul, li {
+    list-style: none;
+}
+.month-selector {
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    padding: 0;
+    border: 1px solid #DFD4A7;
+    font-size: 24px;
+    li {
+        flex: 1;
+        height: 70px;
+        background: #DED5A7;
+        border: 1px solid transparent;
+        border-right: 1px solid #fff;
+        text-align: center;
+        line-height: 70px;
+        color: #fff;
+        cursor: pointer;
+        &:last-child {
+            border-right: 0 solid #fff;
+        }
+        &.prev {
+            background: #F1EEDE;
+        }
+        &.current {
+            background: #fff;
+            color: #142343;
+        }
+        &.active, &:hover {
+            background: #2196f3;
+        }
+    }
+}
+</style>
