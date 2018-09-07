216eff9c1fcdb5b87e1a2eb70ab4064b315ca29e
slaYer <491397425@qq.com>
2018-09-07 09:31:33 +0800

    [fix] vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,3 @@
 module.exports = {
-  configureWebpack: config => {
-    if (process.env.NODE_ENV === 'production') {
-      // 为生产环境修改配置...
-    } else {
-      // 为开发环境修改配置...
-    }
-  }
+  baseUrl: '/hanzoyarest/',
 }
